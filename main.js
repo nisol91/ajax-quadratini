be118ff4e0b9d99fd10ce8dcf5f9b3e1c5080129
nisol91 <nicolasolzi91@gmail.com>
2018-12-12 14:34:31 +0100

    3

--- main.js
+++ main.js
@@ -1,75 +1,81 @@
 //----------------------------------------------
-//CHIAMATA AJAX ALL API
+//CHIAMATA AJAX ALL API (vedi anche il LIVECODING)
 
+$(document).ready(function() {
 
-//modo I : togliendo async(sconsigliato)
+//modo I METTO TUTTA LA GENERAZIONE DEI NUMERI
+//DENTRO AL SUCCESS!
 
-function random_number() {
-  var cpu_n
-  $.ajax({
-    url: 'https://www.boolean.careers/api/random/int',
-    async: false,
-    type: 'GET',
-    success: function(data) {
-      console.log("success");
-      cpu_n = data.response;
-    },
-    error: function() {
-      console.log("error");
-    }
-  });
-  return cpu_n
-}
+//devo sempre mettere tutto dentro a success perche
+//cosi sono sicuro che tutte le operazioni me le fara
+//solo una volta che la chiamata al server e' avvenuta.
+//purtroppo basta un ritardo anche di solo 5ms che se
+//il resto del codice js e' indipendente dalla chiamata,
+//js non legge la chiamata perche e' in ritardo
+//e quindi mi da undefined.
 
-//modo II callback function
-//
-// function random_number(cpu_n) {
-//   var cpu_n
-//   $.ajax({
-//     url: 'https://www.boolean.careers/api/random/int',
-//     type: 'GET',
-//     success: function (data) {
-//       console.log("success");
-//       cpu_n = data.response;
-//       return cpu_n
-//     },
-//     error: function() {
-//       console.log("error");
-//     }
-//   });
-// }
+  //prima genero la mia griglia
+  var righe = parseInt(prompt('Quante righe?'))
+  var colonne = parseInt(prompt('Quante colonne?'))
+  var clone = $('.templates .elem').clone();
+  var numbers = []
 
+  function grid_maker(x, y) {
+    for (var i = 0; i < x; i++) {
+      for (var j = 0; j < y; j++) {
+        var copy = $(clone).clone();
+        $('.griglia').append(copy)
 
-
-
-
-//---------------------------
-//CREAZIONE GRIGLIA nxn
-
-
-var righe = parseInt(prompt('Quante righe?'))
-var colonne = parseInt(prompt('Quante colonne?'))
-var clone = $('.templates .elem').clone();
-var numbers = []
-
-function grid_maker(x, y) {
-  for (var i = 0; i < x; i++) {
-    for (var j = 0; j < y; j++) {
-      var copy = $(clone).clone();
-      $('.griglia').append(copy)
-      var number = random_number();
-      $(copy).find('h1').text(number)
-      console.log(number);
-      if (number <= 5) {
-        $(copy).css('background', 'yellow');
-      } else if (number > 5) {
-        $(copy).css('background', 'green');
       }
     }
+    $('.elem').width(370/x)
+    $('.elem').height(370/y)
   }
-  $('.elem').width(370/x)
-  $('.elem').height(370/y)
-}
+  var tabella = grid_maker(righe, colonne)
 
+  //poi al click genero i numeri con la chiamata ajax
+  //sempre meglio usare il document on quando genero elementi appendendo
+  $(document).on('click', '.elem', function() {
+    // alert('ok')
+    var cpu_n
+    var mioElem = $(this)
+    $.ajax({
+      url: 'https://www.boolean.careers/api/random/int',
+      type: 'GET',
+      success: function(data) {
+        console.log("success");
+        cpu_n = data.response;
+        console.log(cpu_n);
+        mioElem.find('h1').text(cpu_n)
+        if (cpu_n <= 5) {
+          mioElem.css('background', 'yellow');
+        } else if (cpu_n > 5) {
+          mioElem.css('background', 'green');
+        }
+      },
+      error: function() {
+        console.log("error");
+      }
+    });
+  });
 
-var tabella = grid_maker(righe, colonne)
+  //modo II : togliendo async(sconsigliato)
+
+  // function random_number() {
+  //   var cpu_n
+  //   $.ajax({
+  //     url: 'https://www.boolean.careers/api/random/int',
+  //     async: false,
+  //     type: 'GET',
+  //     success: function(data) {
+  //       console.log("success");
+  //       cpu_n = data.response;
+  //     },
+  //     error: function() {
+  //       console.log("error");
+  //     }
+  //   });
+  //   return cpu_n
+  // }
+
+});
